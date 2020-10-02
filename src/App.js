84bf569116333aaadbc402dbac4c5bb05a4cb3bb
ITT <jcromerohdz@gmail.com>
2020-10-02 13:57:51 -0700

    Adding a styled Button component via emotion styled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor:pointer;
+    background-size: 400px;
+  }
+`
 
 function App() {
   return (
-    <h1>Bracking Bad</h1>
+    <Button>
+      Get Quote
+    </Button>
   );
 }
 
